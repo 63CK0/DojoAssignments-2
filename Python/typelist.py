0e5a9124e637c994c4363907b49c66c1ef0288f1
63CK0 <Xecko@protonmail.com>
2018-05-02 10:12:25 -0700

    typelist.py

--- Python/typelist.py
+++ Python/typelist.py
@@ -1,15 +1,24 @@
-l = ['magical unicorns',19,'hello',98.98,'world']
+mixedl = ['magical unicorns',19,'hello',98.98,'world']
 numList = []
 strList = []
 
-def type(object):
-	for x in object:
-		if x < len(object):
-			if object is int:
-				numList.append(object)
-			elif object is str:
-				strList.append(object)
+def listType(lst):
+	empty_string = ''
+	total = 0
+	for x in lst:
+		if isinstance(x,int) or isinstance(x,float):
+			total += x
+		elif isinstance(x,str):
+			empty_string += x
+	if empty_string and total:
+		print "This list you entered is of mixed type"
+		print "String: ", empty_string
+		print "Sum: ", total
+	elif total:
+		print "The list you entered is of integer type"
+		print "Sum: ", total
+	else:
+		print "The list you entered is of string type"
+		print "String: ", empty_string
 
-	print sum(numList)
-	print ' '.join(strList)
-type(l)
+listType(mixedl)
