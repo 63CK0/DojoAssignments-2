dea90ff2d1813acbef32d2be3084787bf45fc905
63CK0 <Xecko@protonmail.com>
2018-05-03 11:24:35 -0700

    fixed counter

--- Python/cointoss.py
+++ Python/cointoss.py
@@ -1,16 +1,16 @@
 import random
 
 def coin_toss():
+	tails =0
+	heads = 0
 	for i in range(0,5000):
 		x = random.random()
-		x_rounded = round(x)
-		tails =0
-		heads = 0 
+		x_rounded = round(x) 
 		if x_rounded == 0:
-			 tails += 1
+			tails += 1
 			
 			print "tails:", tails
-		elif x_rounded == 1:
+		else:
 			heads += 1
 			print "heads:", heads
 		
