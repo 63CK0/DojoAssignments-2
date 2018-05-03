50eb5d95e9bcccd9504b1e04e8e394bf7a3a1862
63CK0 <Xecko@protonmail.com>
2018-05-02 22:30:42 -0700

    multiplication table with proper spacing, use indent for proper spacing

---
+++ Python/mult-table.py
@@ -0,0 +1,13 @@
+print "\t\tMultiplication Table \n"
+
+for i in range(1,13):
+	print i,  '	',
+print
+print '*' * 100, '\n'
+for j in range(1,13):
+	for k in range (1,13):
+		print j * k, '	',
+
+	print '\t'
+	print '\n'
+print '*' * 100, '\n'
