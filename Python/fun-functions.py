925d7823d6a5da33c44ee3823889b13764afc3d4
63CK0 <Xecko@protonmail.com>
2018-05-03 10:07:47 -0700

    fun with functions /hack

--- Python/fun-functions.py
+++ Python/fun-functions.py
@@ -18,11 +18,15 @@ print b
 
 
 def layered_multiples(arr):
-	for x in range(len(arr)):
-		arr[x] == 1
-		return new_array
+	new_array = []
+	for x in arr:
+		val_arr = []
+		for i in range(0,x):
+			val_arr.append(1)
+		new_array.append(val_arr)
+	return new_array
+
 x = layered_multiples(multiply([2,4,5],3))
 print x
-
 
 	
