ed518c117815181da5e2d4a2c0ddc12d1e45e52c
63CK0 <Xecko@protonmail.com>
2018-05-03 21:04:51 -0700

    stars part II

--- Python/stars.py
+++ Python/stars.py
@@ -1,5 +1,12 @@
-x = [4, 6, 1, 3, 5, 7, 25]
+
 def stars(lst):
 	for x in lst:
-		print '*'*x
+		if isinstance(x, int or float):
+			print '*'*x
+		if isinstance(x, str):
+			length = len(x)
+			letter = x[0].upper()
+			print letter * length
+
+x = [4, "Tom", 1, "Michael", 5, 7, "Jimmy Smith"]
 stars(x)
