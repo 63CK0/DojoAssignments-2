a1d8a68df48f0cc42d82696695d50cf11691f0d0
63CK0 <Xecko@protonmail.com>
2018-05-03 21:43:15 -0700

    updated names to easier list traversal

--- Python/names.py
+++ Python/names.py
@@ -1,13 +1,11 @@
-students = {
-	'names': [
+students =[
      {'first_name':  'Michael', 'last_name' : 'Jordan'},
      {'first_name' : 'John', 'last_name' : 'Rosales'},
      {'first_name' : 'Mark', 'last_name' : 'Guillen'},
      {'first_name' : 'KB', 'last_name' : 'Tonel'}
-	]
-}
-def names():
-	for key, data in students.items():
-		for value in data:
-			print value["first_name"], value["last_name"]
-names()
+     ]
+def pstudent():
+	for x in students:
+		print x["first_name"], x["last_name"]
+
+pstudent()
