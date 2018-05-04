6401f93bf7c1bc6c032b4eb6cbb5649165d6a82f
63CK0 <Xecko@protonmail.com>
2018-05-04 01:15:07 -0700

    names and dictionaries

--- Python/names.py
+++ Python/names.py
@@ -1,11 +1,24 @@
-students =[
+users = {
+ 'Students': [
      {'first_name':  'Michael', 'last_name' : 'Jordan'},
      {'first_name' : 'John', 'last_name' : 'Rosales'},
      {'first_name' : 'Mark', 'last_name' : 'Guillen'},
      {'first_name' : 'KB', 'last_name' : 'Tonel'}
-     ]
-def pstudent():
-	for x in students:
-		print x["first_name"], x["last_name"]
+  ],
+ 'Instructors': [
+     {'first_name' : 'Michael', 'last_name' : 'Choi'},
+     {'first_name' : 'Martin', 'last_name' : 'Puryear'}
+  ]
+ }
+def show_name():
+ 	for job in users:
+ 		counter = 0
+ 		print job
+ 		for person in users[job]:
+ 			counter += 1
+ 			first_name = person["first_name"]
+ 			last_name = person["last_name"]
+ 			length = len(first_name) + len(last_name)
+ 			print "{}-{} {}-{}".format(counter,first_name,last_name,length)
 
-pstudent()
+show_name()
