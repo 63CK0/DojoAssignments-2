b13d45785683d64a9d6fabb0881fde5d6beb02c3
63CK0 <Xecko@protonmail.com>
2018-05-03 21:25:56 -0700

    dictionary practice

---
+++ Python/dict.py
@@ -0,0 +1,7 @@
+person = {"Name": "Anna", "Age": "101", "Country": "The United States", "Language": "Python"}
+def anna():
+	print "My name is", person["Name"]
+	print "My age is", person["Age"]
+	print "My count of birth is", person["Country"]
+	print "My favorite language is", person["Language"]
+anna()
